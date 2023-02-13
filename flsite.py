bcb5c5b8276edd5aaaba1ddf864020d56124c508
Вячеслав Гавдзинский <veacheslavgavdzynsky@gmail.com>
2023-02-13 16:40:26 +0200

    Lesson 4

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,12 +8,19 @@ menu = ["Установка", "Первое приложение", "Обратн
 
 @app.route("/")
 def index():
+    print(url_for("index"))
     return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
 def about():
+    print(url_for("about"))
     return render_template("about.html", title="О сайте", menu=menu)
+
+
+@app.route("/profile/<username>")
+def profile(username, path):
+    return f"Пользователь: {username}, {path}"
 
 
 if __name__ == "__main__":
