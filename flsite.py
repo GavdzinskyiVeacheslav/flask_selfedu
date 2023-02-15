c1392d27ee25086f31de02f61048e8858dbd9adb
Вячеслав Гавдзинский <veacheslavgavdzynsky@gmail.com>
2023-02-15 13:32:33 +0200

    Lesson 6

--- flsite.py
+++ flsite.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thwrthsjdldnahsrtb'
 
 
 menu = [{"name": "Установка", "url": "install-flask"},
@@ -23,8 +24,13 @@ def about():
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == 'POST':
-        print(request.form)
-    return render_template("contact.html", title="Обратная связь", menu = menu)
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash("Ошибка отправки", category='error')
+        print(request.form['username'])
+
+    return render_template("contact.html", title="Обратная связь", menu=menu)
 
 
 # @app.route("/profile/<username>")
