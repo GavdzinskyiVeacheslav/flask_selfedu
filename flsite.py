74b1ddffd45188862e1414f6478c237f0a190cd6
Вячеслав Гавдзинский <veacheslavgavdzynsky@gmail.com>
2023-02-16 14:42:48 +0200

    Lesson 7

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thwrthsjdldnahsrtb'
@@ -32,6 +32,29 @@ def contact():
 
     return render_template("contact.html", title="Обратная связь", menu=menu)
 
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f"Профиль пользователя: {username}"
+
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template("page404.html", title='Страница не найдена', menu=menu), 404
 
 # @app.route("/profile/<username>")
 # def profile(username, path):
