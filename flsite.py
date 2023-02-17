d8274405b6fb35e02d27c92eef59a5d21f8b901d
Вячеслав Гавдзинский <veacheslavgavdzynsky@gmail.com>
2023-02-17 12:37:27 +0200

    Lesson 8

--- flsite.py
+++ flsite.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
+import sqlite3
+import os
+
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'vasdnianvsdnfviuvjvfn'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thwrthsjdldnahsrtb'
+app.config.from_object(__name__) #['SECRET_KEY'] = 'thwrthsjdldnahsrtb'
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
 
 
 menu = [{"name": "Установка", "url": "install-flask"},
@@ -11,8 +34,21 @@ menu = [{"name": "Установка", "url": "install-flask"},
 
 @app.route("/")
 def index():
-    print(url_for("index"))
-    return render_template("index.html", menu=menu)
+    db = get_db
+   # print(url_for("index"))
+    return render_template("index.html", menu = [])
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
 
 
 @app.route("/about")
